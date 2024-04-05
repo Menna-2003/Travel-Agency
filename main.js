3fc977e2ad72c35f43e1cd9d4dd136d70cee9bdb
Menna Mohamed <2022170446@cis.asu.edu.eg>
2024-04-06 01:01:32 +0200

    edit responsive

--- main.js
+++ main.js
@@ -2,23 +2,45 @@ let right = document.getElementsByClassName("right")[0];
 let left = document.getElementsByClassName("left")[0];
 let content = document.getElementsByClassName("content")[0];
 
-width = -1 * (content.offsetWidth - 640);
+if (window.innerWidth > 780) {
+    width = -1 * (content.offsetWidth - 640);
+    let leftspace = 0;
+    right.addEventListener("click", (e) => {
+        if (leftspace >= width) {
+            leftspace -= 320;
+            console.log(leftspace);
+            let variable = `${leftspace}px`;
+            content.style.left = variable;
+        }
+    });
 
-let leftspace = 0;
-right.addEventListener("click", (e) => {
-    if (leftspace >= width) {
-        leftspace -= 320;
-        console.log(leftspace);
-        let variable = `${leftspace}px`;
-        content.style.left = variable;
-    }
-});
+    left.addEventListener("click", (e) => {
+        if (leftspace !== 0) {
+            leftspace += 320;
+            console.log(leftspace);
+            let variable = `${leftspace}px`;
+            content.style.left = variable;
+        }
+    });
+}
+else{
+    width = -1 * (content.offsetWidth - 500);
+    let leftspace = 0;
+    right.addEventListener("click", (e) => {
+        if (leftspace >= width) {
+            leftspace -= 260;
+            console.log(leftspace);
+            let variable = `${leftspace}px`;
+            content.style.left = variable;
+        }
+    });
 
-left.addEventListener("click", (e) => {
-    if (leftspace !== 0) {
-        leftspace += 320;
-        console.log(leftspace);
-        let variable = `${leftspace}px`;
-        content.style.left = variable;
-    }
-});
+    left.addEventListener("click", (e) => {
+        if (leftspace !== 0) {
+            leftspace += 260;
+            console.log(leftspace);
+            let variable = `${leftspace}px`;
+            content.style.left = variable;
+        }
+    });
+}
