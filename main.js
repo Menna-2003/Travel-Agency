a7933b598e378bc62a7925fb4d0bac0cdb52377f
Menna Mohamed <2022170446@cis.asu.edu.eg>
2024-05-09 23:38:32 +0300

    ...

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 let right = document.getElementsByClassName("right")[0];
 let left = document.getElementsByClassName("left")[0];
 let content = document.getElementsByClassName("content")[0];
+let bike = document.querySelector(".ReservationContainer img.bike");
+let bikebtn = document.querySelector(".ReservationContainer .btn.bike");
+let car = document.querySelector(".ReservationContainer img.car");
+let carbtn = document.querySelector(".ReservationContainer .btn.car");
+let title = document.querySelector(".ReservationContainer .title");
+
+carbtn.addEventListener("click", (e) => {
+    carbtn.classList.add("active");
+    bikebtn.classList.remove("active");
+    bike.style.visibility = "hidden";
+    car.style.visibility = "visible";
+    title.innerHTML = "Book Now Car";
+});
+bikebtn.addEventListener("click", (e) => {
+    bikebtn.classList.add("active");
+    carbtn.classList.remove("active");
+    car.style.visibility = "hidden";
+    bike.style.visibility = "visible";
+    title.innerHTML = "Book Now Bike";
+});
 
 if (window.innerWidth > 780) {
-    width = -1 * (content.offsetWidth - 640);
+    width = -1 * (content.offsetWidth - 400);
     let leftspace = 0;
     right.addEventListener("click", (e) => {
         if (leftspace >= width) {
@@ -22,9 +42,8 @@ if (window.innerWidth > 780) {
             content.style.left = variable;
         }
     });
-}
-else{
-    width = -1 * (content.offsetWidth - 500);
+} else {
+    width = -1 * (content.offsetWidth - 800);
     let leftspace = 0;
     right.addEventListener("click", (e) => {
         if (leftspace >= width) {
